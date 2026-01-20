ec8ff4d533e0f55987ec85a2498335aac7eb1e88
ferlicio <ferlicio@gmail.com>
2026-01-20 16:34:50 -0300

    add base entity and auditable entity

---
+++ src/main/java/com/example/JavaSpringBase/models/base/AuditableEntity.java
@@ -0,0 +1,42 @@
+package com.example.JavaSpringBase.models.base;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+// import org.springframework.data.annotation.CreatedBy;
+// import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass // Indica que esta classe é uma superclasse mapeada
+@EntityListeners(AuditingEntityListener.class) // Habilita o Listener de auditoria do JPA
+@Getter @Setter
+public abstract class AuditableEntity extends BaseEntity { // Usada para Entidades que precisam de auditoria (criação e modificação)
+
+    // Para a hora e data de criação e modificação, você pode usar Instant ou LocalDateTime dependendo das suas necessidades
+    // Caso o banco de dados suporte fuso horário, Instant é uma boa escolha
+    // Caso contrário, LocalDateTime pode ser mais apropriado
+    
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private Instant createdAt;
+
+    // @CreatedBy
+    // @Column(name = "created_by", updatable = false, nullable = false)
+    // private String createdBy;
+    
+    @LastModifiedDate
+    @Column(name = "updated_at", updatable = true, nullable = true)
+    private Instant updatedAt;
+
+
+    // @LastModifiedBy
+    // @Column(name = "updated_by", updatable = true, nullable = true)
+    // private String updatedBy;
+}
---
+++ src/main/java/com/example/JavaSpringBase/models/base/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.example.JavaSpringBase.models.base;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass // Indica que esta classe é uma superclasse mapeada
+@Getter @Setter
+public abstract class BaseEntity { // Usada para Entidades que precisam de um ID padrão
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    // use GenerationType.IDENTITY caso seja um sistema pequeno e centralizado 
+    // ou use GenerationType.UUID caso seja um sistema grande, que rode em microserviços ou por segurança
+    private Long id; // ou UUID 
+}
