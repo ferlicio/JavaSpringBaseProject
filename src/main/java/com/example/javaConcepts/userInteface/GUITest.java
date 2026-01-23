6e9cd79612e4d8b720c6841c0e260733ceedf858
ferlicio <ferlicio@gmail.com>
2026-01-23 17:17:53 -0300

    User Interface example

---
+++ src/main/java/com/example/javaConcepts/userInteface/GUITest.java
@@ -0,0 +1,12 @@
+package com.example.javaConcepts.userInteface;
+
+import javax.swing.*;
+public class GUITest {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Exemplo");
+        // JButton botao = new JButton("Clique aqui");
+        // frame.add(botao);
+        frame.setSize(300,200);
+        frame.setVisible(true);
+    }
+}
